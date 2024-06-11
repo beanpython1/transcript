2c152c758957f0a4f6277adca52ef0030e82b596
bean <125648438+beanpython1@users.noreply.github.com>
2024-06-11 12:23:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,161 +1,78 @@
-document.getElementById("youtubeLink").addEventListener("input", function() {
-    var youtubeLink = document.getElementById("youtubeLink").value.trim();
-    var isDisabled = youtubeLink === "";
-    ["getTranscriptButton", "getPDFButton", "getVIDButton", "getAIButton", "copyButton", "getMP3Button"].forEach(id => {
-        document.getElementById(id).disabled = isDisabled;
-    });
-});
-document.getElementById("copyButton").addEventListener("click", function() {
-    var transcriptElement = document.getElementById("transcript");
-    var textToCopy = transcriptElement.innerText;
-
-    var tempTextArea = document.createElement("textarea");
-    tempTextArea.value = textToCopy;
-    document.body.appendChild(tempTextArea);
-
-    tempTextArea.select();
-    document.execCommand("copy");
-
-    document.body.removeChild(tempTextArea);
-
-    alert("Copied the transcript/summary to clipboard!");
-});
-
-function fetchTranscript(youtubeLink) {
-    var videoID = youtubeLink.split('v=')[1];
-    if (!videoID) throw new Error('Invalid YouTube link');
-    var apiUrl = 'https://youtube-transcriber-api.vercel.app/v1/transcripts?id=' + videoID + '&lang=en';
-    return fetch(apiUrl).then(response => {
-        if (!response.ok) throw new Error('Network response was not ok');
-        return response.json();
-    });
+body {
+    background-image: url('background.jpg');
+    background-size: cover;
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
 }
 
-function displayError(message) {
-    document.getElementById("transcript").innerText = message;
+.container {
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    text-align: center;
 }
 
-function getTranscript() {
-    var youtubeLink = document.getElementById("youtubeLink").value;
-    fetchTranscript(youtubeLink)
-        .then(data => {
-            var transcript = data.transcripts[0].text;
-            document.getElementById("transcript").innerText = transcript;
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-            displayError("Error retrieving transcript. Reasons could be the language is not in English, incorrect video ID, or API issues.");
-        });
+h1 {
+    color: red;
+    font-size: 2em;
 }
 
-function getPDF() {
-    var youtubeLink = document.getElementById("youtubeLink").value;
-    fetchTranscript(youtubeLink)
-        .then(data => {
-            var transcript = data.transcripts[0].text;
-            document.getElementById("transcript").innerText = transcript;
-
-            const { jsPDF } = window.jspdf;
-            var doc = new jsPDF();
-            var pageHeight = doc.internal.pageSize.height;
-            var margin = 10;
-            var maxLineWidth = 180;
-            var lineHeight = 10;
-            var y = margin;
-
-            var lines = doc.splitTextToSize(transcript, maxLineWidth);
-            lines.forEach(line => {
-                if (y + lineHeight > pageHeight - margin) {
-                    doc.addPage();
-                    y = margin;
-                }
-                doc.text(line, margin, y);
-                y += lineHeight;
-            });
-
-            doc.save('transcript.pdf');
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-            displayError("Error retrieving transcript");
-        });
+#transcript {
+    margin-top: 60px;
+    padding: 20px;
+    border: 2px solid red;
+    background-color: #f1f1f1;
+    border-radius: 15px;
+    color: #333;
+    width: 80%;
+    max-width: 800px;
+    min-height: 350px;
+    max-height: 450px;
+    overflow-y: auto;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    margin-left: auto;
+    margin-right: auto;
 }
 
-
-// Client-side JavaScript
-function getVID() {
-    var URLInput = document.getElementById("youtubeLink").value;
-    sendURL(URLInput); // Call sendURL with the input value directly, without calling .value again
+input[type="text"] {
+    margin: 10px;
+    padding: 5px;
+    background-color: red;
+    border: 2px white dotted;
+    border-radius: 5px;
+    color: white;
 }
 
-function getMP3() {
-    var URLInput = document.getElementById("youtubeLink").value;
-    sendMP3(URLInput); // Call sendMP3 with the input value directly, without calling .value again
+::placeholder {
+    color: white;
 }
 
-function sendMP3(URL) {
-    window.location.href = `https://youtube-transcript-8nb1.onrender.com/downloadmp3?URL=${encodeURIComponent(URL)}`; // Encode the URL before sending it to the server
+button {
+    margin-top: 10px;
+    background-color: red;
+    padding: 5px 10px;
+    color: white;
+    border-radius: 15px;
+    border: 1px black solid;
+    font-size: 1em;
 }
 
-function sendURL(URL) {
-    window.location.href = `https://youtube-transcript-8nb1.onrender.com/download?URL=${encodeURIComponent(URL)}`; // Encode the URL before sending it to the server
+button:hover {
+    cursor: pointer;
 }
 
-
-
-function getAI() {
-    var youtubeLink = document.getElementById("youtubeLink").value;
-    fetchTranscript(youtubeLink)
-        .then(data => {
-            var transcript = data.transcripts[0].text;
-            var cleanedTranscript = transcript.replace(/\[\s*__\s*\]/g, ''); // Remove [ __ ]
-            var isTrimmed = cleanedTranscript.length > 5100;
-            var trimmedTranscript = isTrimmed ? cleanedTranscript.slice(0, 5100) : cleanedTranscript;
-            console.log(trimmedTranscript)
-
-            return fetch('https://youtube-transcript-8nb1.onrender.com/summarize', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ transcript: trimmedTranscript })
-            }).then(response => {
-                if (!response.ok) throw new Error('Network response was not ok');
-                return response.json();
-            }).then(summary => {
-                var summaryText = isTrimmed ? summary.summary + ' (Only summarised first 5100 characters)' : summary.summary;
-                document.getElementById("transcript").innerText = summaryText;
-            });
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-            displayError("Error summarizing transcript");
-        });
+button:disabled {
+    cursor: default;
+    background-color: #666;
+    color: #ccc;
 }
 
-
-function getAIText() {
-    var userPrompt = prompt("Enter AI Prompt: ");
-
-    if (userPrompt) {
-        fetch('https://youtube-transcript-8nb1.onrender.com/prompt', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ prompt: userPrompt })
-        })
-        .then(response => {
-            if (!response.ok) throw new Error('Network response was not ok');
-            return response.json();
-        })
-        .then(data => {
-            var reply = data.reply; // Assuming the server response has a 'reply' field
-            document.getElementById("transcript").innerText = reply;
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-            displayError("Error processing AI prompt");
-        });
-    }
+#getAI {
+    position: fixed;
+    left: 50%;
+    bottom: -8em;
+    transform: translateX(-50%);
+    z-index: 9999;
 }
